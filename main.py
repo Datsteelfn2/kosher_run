b56fafe7268b1e24a6b7e595aa9f3bb0ee726666
Datsteelfn2 <Datsteelfn@gmail.com>
2024-12-29 07:31:33 -0500

    made the player pop up on the screen

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ platform_surface=pygame.image.load("images/platform.png").convert_alpha()
 platform_surface=pygame.transform.scale(platform_surface,(800,200))
 yellow=(220, 193, 185)
 
+player_surface=pygame.image.load("images/blp.png")
+player_surface=pygame.transform.scale(player_surface,(300,150))
+player_rect=player_surface.get_rect(midbottom=(50,336))
+
+
+
 run=True
 while run:
     for event in pygame.event.get():
@@ -14,6 +20,8 @@ while run:
             run=False
     screen.fill(yellow)
     screen.blit(platform_surface,(0,300))
+    screen.blit(player_surface,player_rect)
+    
     clock.tick(60)
 
     pygame.display.update()
