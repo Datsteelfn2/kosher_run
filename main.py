445b07b9a99461cdee79178ed1e867b37c10e0a7
Datsteelfn2 <Datsteelfn@gmail.com>
2024-12-29 07:35:42 -0500

    implemented gravity

--- main.py
+++ main.py
@@ -9,16 +9,22 @@ yellow=(220, 193, 185)
 
 player_surface=pygame.image.load("images/blp.png")
 player_surface=pygame.transform.scale(player_surface,(300,150))
-player_rect=player_surface.get_rect(midbottom=(50,336))
+player_rect=player_surface.get_rect(midbottom=(150,336))
 
 
-
+gravity=0
 run=True
 while run:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             run=False
+        if event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_SPACE:
+                gravity=-20
+
     screen.fill(yellow)
+    gravity+=1
+    player_rect.y+=gravity
     screen.blit(platform_surface,(0,300))
     screen.blit(player_surface,player_rect)
     
