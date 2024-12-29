f84e47e6749c60a6614a4203910a5fd920a12809
Datsteelfn2 <Datsteelfn@gmail.com>
2024-12-29 07:20:21 -0500

    added more accurate images, transparent, and set up the screen for now

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ pygame.init()
 screen=pygame.display.set_mode((800,400))
 
 clock=pygame.time.Clock()
+platform_surface=pygame.image.load("images/platform.png").convert_alpha()
+platform_surface=pygame.transform.scale(platform_surface,(800,200))
+yellow=(220, 193, 185)
 
 run=True
 while run:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             run=False
+    screen.fill(yellow)
+    screen.blit(platform_surface,(0,300))
     clock.tick(60)
+
     pygame.display.update()
