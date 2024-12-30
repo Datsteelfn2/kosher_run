33b5b7df88d0b87db2ac69c50850bd26de57d6c8
Datsteelfn2 <Datsteelfn@gmail.com>
2024-12-30 02:08:41 -0500

    Fixed losing screen bug where the score kept running

--- main.py
+++ main.py
@@ -30,11 +30,12 @@ gravity=0
 font=pygame.font.Font(None,50)
 menu_font=pygame.font.Font(None,40)
 def display_score():
-    time=pygame.time.get_ticks()-start_time
-    text=font.render(str(time),0,"Black")
-    text_rect=text.get_rect(center=(WIDTH/2,50))
-    screen.blit(text,text_rect)
-    return time
+    if game_activity and not game_over:
+        time=int(pygame.time.get_ticks()/1000)-start_time
+        text=font.render(f"SCORE:{time}",0,"Black")
+        text_rect=text.get_rect(center=(WIDTH/2,50))
+        screen.blit(text,text_rect)
+        return time
 start_time=0
 #menu screen 
 def menu():
@@ -54,23 +55,28 @@ def menu():
     screen.blit(context_game_text2,context_game_rect2)
     screen.blit(start_text,start_rect)
 
-def losing_screen():
+def losing_screen(final_score):
     screen.fill("White")
-    time=display_score()
+    time=final_score
     losing_text=font.render("Thanks for playing,You lost",0,"Black")
     score_text=font.render(f"Your Score:{time}",0,"Red")
+    play_text=font.render("Press Space to play again!",0,"Black")
+    play_rect=play_text.get_rect(center=(WIDTH/2,150))
+
     losing_rect=losing_text.get_rect(center=(WIDTH/2,50))
     score_rect=score_text.get_rect(center=(WIDTH/2,100))
     screen.blit(losing_text,losing_rect)
     screen.blit(score_text,score_rect)
+    screen.blit(play_text,play_rect)
 game_activity=False
 game_over=False
+final_score=0
 
 
 run=True
 while run:
     screen.fill(yellow)
-    if game_activity==False and not game_over:
+    if not game_activity and not game_over:
         menu()
    
     for event in pygame.event.get():
@@ -82,42 +88,49 @@ while run:
                 if event.type==pygame.KEYDOWN:
                     if event.key==pygame.K_SPACE:
                         gravity=-25
-        if game_activity==False:
-            if  game_over==True:
-                start_time=int(pygame.time.get_ticks()/1000)
+        if not game_activity and not game_over:
+            
             if event.type== pygame.KEYDOWN:
                 if event.key==pygame.K_SPACE:
+                    
                     game_activity=True
-    
+                    start_time=int(pygame.time.get_ticks()/1000)
+        if game_over:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_activity = True
+                game_over = False
+                final_score=0
+                start_time = int(pygame.time.get_ticks() / 1000)
+                gravity = 0
+                player_rect.midbottom = (150, 346)
+                enemy_rect.midbottom = (700, 346)
     #intitial menu screen
 
     #gravity
-    gravity+=1
-    player_rect.y+=gravity
+    if game_activity:
+        gravity+=1
+        player_rect.y+=gravity
 
-    if player_rect.bottom>336:
-        player_rect.bottom=336
-    #infinite movement
-    platform_x1-=platform_speed#makes the platform move to the left
-    platform_x2-=platform_speed
-    # check and see if the platform moves off the screen
-    if platform_x1 <= -WIDTH:  # if less than -800
-        platform_x1 = 800  # Place platform 1 just after platform 2
+        if player_rect.bottom>336:
+            player_rect.bottom=336
+        #infinite movement
+        platform_x1-=platform_speed#makes the platform move to the left
+        platform_x2-=platform_speed
+        # check and see if the platform moves off the screen
+        if platform_x1 <= -WIDTH:  # if less than -800
+            platform_x1 = 800  # Place platform 1 just after platform 2
 
-# Check if platform 2 has moved completely off the screen
-    if platform_x2 <= -WIDTH:
-        platform_x2 = 800  # Place platform 2 just after platform 1
+    # Check if platform 2 has moved completely off the screen
+        if platform_x2 <= -WIDTH:
+            platform_x2 = 800  # Place platform 2 just after platform 1
 
     #constantly drawing the platform, the platform changes because were constantly adding platform_speed to platform_x1 and platform_x2"
-
-    if game_activity==True:
-        
         screen.blit(platform_surface,(platform_x1,300))
         screen.blit(platform_surface,(platform_x2,300))
         screen.blit(player_surface,player_rect)
-        display_score()
+        final_score=display_score()
         # enemy logic
-        enemy_rect.x-=4
+        enemy_rect.x-=7
         screen.blit(enemy_surface,enemy_rect)
         if enemy_rect.x<=0:
             enemy_rect.x=800
@@ -127,8 +140,11 @@ while run:
         if player_rect.colliderect(enemy_rect):
             game_activity=False
             game_over=True
+    
+        
+        
             
-    if game_over==True:
-        losing_screen()
+    if game_over:
+        losing_screen(final_score)
     clock.tick(60)
     pygame.display.update()
